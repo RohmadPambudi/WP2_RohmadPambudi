1d14e507eca2757417261ea33a7c22cbc0fa3eff
RohmadPambudi <rohmadsenpai@gmail.com>
2022-03-28 13:42:55 +0700

    commit on 'master'

---
+++ Pustaka_Booking/Coding/application/views/view_latihan.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Latihan1</title>
+</head>
+<body>
+    Hallo Kawan.. Yuk kita belajar web programming...!!!!<br>
+    Nilai 1 = <?=$nilai1; ?>
+    Nilai 2 = <?=$nilai2; ?><br>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu 
+    <?php $nilai1. " + " . $nilai2. " = " . $hasil; ?>
+    
+    <?php echo "$nilai1 + $nilai2 = $hasil"; ?>
+</body>
+</html>
