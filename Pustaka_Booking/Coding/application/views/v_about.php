3add2f27016bbeaf65f0fe63e89ad82965f62b48
RohmadPambudi <rohmadsenpai@gmail.com>
2022-04-02 06:54:47 +0700

    web.php,v_header,v_index,v_footer,v_about,add_assets,SS

---
+++ Pustaka_Booking/Coding/application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type= "disc">
+        <li>Nama Depan : Rohmad </li>
+        <li>Nama Belakang : Pambudi </li>
+</ul>
+<br>
+
+<h4>Alamat</h4>
+<ul type= "none">
+    <li> Jl. Sumur Bor</li>
+</ul>
+
+<h4>Tempat Lahir</h4>
+<ul type= "none">
+    <li>Jawa Tengah</li>
+</ul>
+
+<h4>Olahraga Favorit</h4>
+<ul type= "square">
+    <li>Futsal</li>
+    <li>Bulutangkis</li>
+</ul>
+</section>
